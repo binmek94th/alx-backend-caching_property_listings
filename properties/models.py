c427d88dade3012cb662f0ef8b073bc7f0a7690a
Nehemiah Mekonnen <binyamm83@gmail.com>
2025-07-11 22:50:12 +0300

    fixes

--- properties/models.py
+++ properties/models.py
@@ -6,4 +6,4 @@ class Property(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     location = models.CharField(max_length=100)
-    created = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
